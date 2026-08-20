19bfb7770b650c7650da406dc34c5a86c14f8d64
Calvin - Chen - dev <calvinctitan@gmail.com>
2026-08-19 23:08:28 -0400

    Update pose_extractor.py
    I added the angle calculator and changed Mediapipe from lite version to full version

--- pose_extractor.py
+++ pose_extractor.py
@@ -1,3 +1,4 @@
+import numpy as np # introduce arrays in this python so it runs numbers faster
 import cv2   # this line load openCV for the machine to use
 
 def load_video(filepath):
@@ -14,7 +15,7 @@ def load_video(filepath):
     print("Video Not Loaded")
   return cap
 
-video = load_video( "Screen recording 2026-08-11 8.52.00 PM.mp4")
+video = load_video( "Swimtestone.mp4")
 
 
 ret,frame = video.read()  #cap.read will capture the next frame, telling you wether it worked and the image iteslef
@@ -27,7 +28,7 @@ else:
 
 
 
-!wget -q https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_lite/float16/latest/pose_landmarker_lite.task #downloads the AI model mediapipe
+!wget -q https://storage.googleapis.com/mediapipe-models/pose_landmarker/pose_landmarker_full/float16/latest/pose_landmarker_full.task #download the mediapipe
 !pip install mediapipe
 
 import mediapipe as mp
@@ -39,16 +40,32 @@ VisionRunningMode = mp.tasks.vision.RunningMode
 
 
 options = PoseLandmarkerOptions(
-    base_options=BaseOptions(model_asset_path="pose_landmarker_lite.task"),
+    base_options=BaseOptions(model_asset_path="pose_landmarker_full.task"),
     running_mode=VisionRunningMode.IMAGE)
 landmarker = PoseLandmarker.create_from_options(options)
-
-
 # Set up the detector using the model we just downloaded
 
 
-video.set(cv2.CAP_PROP_POS_FRAMES, 30)
-ret, frame = video.read()
+
+
+video = load_video("Swimtestone.mp4")  # reopen fresh — swap in your actual filename
+
+target_frame = 285
+for i in range(target_frame):
+    ret, frame = video.read()
+
+print("Actually on frame:", video.get(cv2.CAP_PROP_POS_FRAMES))
+
+if ret:
+    from google.colab.patches import cv2_imshow
+    cv2_imshow(frame)
+
+
+
+
+
+
+
 
 
 rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
@@ -67,4 +84,27 @@ if result.pose_landmarks:
 else:
   print("No pose landmarks detected.")
 
-#shows the left elbow coordinates. left elbow is always number 13 in Mediapipe  
+#shows the left elbow coordinates. left elbow is always number 13 in Mediapipe
+
+
+
+
+
+def calculate_angle(A,B,C):
+
+  a = np.array([A["x"], A["y"]])
+  b = np.array([B["x"], B["y"]])   # elbow — the middle point
+  c = np.array([C["x"], C["y"]])
+
+  ba = a - b   # arrow from elbow to shoulder
+  bc = c - b   # arrow from elbow to wrist
+  cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+  angle = np.degrees(np.arccos(cosine_angle))  #mediapipe angle calculation formula
+
+  return angle
+
+
+
+
+
+
